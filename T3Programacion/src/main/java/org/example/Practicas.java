fe037ac8cbae5d4990ecb947df0954d97c5358ab
A612\1DAW <davalorib@gmail.com>
2024-11-08 18:53:50 +0100

    practica samurais a falta de que sumen

--- T3Programacion/src/main/java/org/example/Practicas.java
+++ T3Programacion/src/main/java/org/example/Practicas.java
@@ -11,39 +11,65 @@ public class Practicas {
         Scanner ent = new Scanner(System.in);
 
         boolean valido;
+        boolean valido2;
+        boolean si;
         int[] samuraisint = new int[7];
         int[] samurais1int = new int[7];
         int[] samurais2int = new int[7];
         String[] samuraissplit;
 
-        System.out.println("\n Introduce la potencia de tus 7 samurais separadas mediante una ',' : ");
+        Random random = new Random();
 
+        int numrandom = random.nextInt(7);
+        int aux = 0;
 
-        for (int n=1; n<=2; n++) {
-            System.out.println("> Equipo "+ n);
+        System.out.println("\n Introduce la potencia de tus 7 samurais separadas mediante una ','. Tienen que sumar 30 : ");
+
+        for (int n = 1; n <= 2; n++) {
+            System.out.println("> Equipo " + n);
             String samurais = ent.next();
             samuraissplit = samurais.split(",");
 
             if (samuraissplit.length != 7) {
-                System.out.println("ERROR... Escribe 7 potencias");
+                System.out.println("> ERROR... Escribe 7 potencias");
                 n--;
                 continue;
             }
 
             valido = true;
-
             for (int i = 0; i < samuraissplit.length; i++) {
                 try {
                     samuraisint[i] = Integer.parseInt(samuraissplit[i]);
                 } catch (NumberFormatException er) {
-                    System.out.println("ERROR... Introduce nuevamente 7 potencias pero con número válidos");
+                    System.out.println("> ERROR... Introduce nuevamente 7 potencias pero con número válidos");
                     n--;
                     valido = false;
                     break;
                 }
             }
 
-            if (valido) { //si no pongo esta verificacion previa se me clona el primero si el segundo lo hago mal
+            valido2 = true;
+            if (valido) {
+                int suma = 0;
+                for (int i = 0; i < samuraissplit.length; i++) {
+                    suma += samuraisint[i];
+                }
+                if (suma != 30) {
+                    System.out.println("> ERROR... La potencia total no suma 30");
+                    System.out.println("> Introduce nuevamente 7 potencias pero que sumen 30");
+                    n--;
+                    valido2 = false;
+                    continue;
+                }
+            }
+
+            for ( int i=0; i<numrandom; i++ ) {
+                aux = samuraisint[i];
+                samuraisint[i] = samuraisint[samuraisint.length-i-1];
+                samuraisint[samuraisint.length-i-1] = aux;
+            }
+
+            if (valido2) { //si no pongo esta verificacion previa se me clona el primero si el segundo lo hago mal
                 if (n == 1) {
                     samurais1int = samuraisint.clone();
                 } else if (n == 2) {
@@ -51,10 +77,6 @@ public class Practicas {
                 }
             }
         }
-
-        Random random = new Random();
-
-        int numrandom = random.nextInt(7);
 
         System.out.println(Arrays.toString(samurais1int));
         System.out.println(Arrays.toString(samurais2int));
